03e4c0d50044aab64df43b1548f44d7495665ba8
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-29 16:49:40 +0200

    Modified web page for single-joystick

---
+++ components/single/web/index.html.js
@@ -0,0 +1,21 @@
+import getNipplejsMin from "react-native-game-pad/components/single/web/nipplejs.min.js";
+
+const getWebPageSingleJoystick = () => {
+  const nipplejs = getNipplejsMin();
+  return `
+    <html>
+      <body>
+        <div id="zone_joystick" style="
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            left: 0;"></div>
+      </body>
+      <script>
+        ${nipplejs}
+      </script>
+    </html>
+    `;
+}
+
+export default getWebPageSingleJoystick;
