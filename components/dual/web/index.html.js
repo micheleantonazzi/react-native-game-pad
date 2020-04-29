d85dda875ecdeecb1d36dff4ec6f501baf861588
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-29 16:23:54 +0200

    Modified web page for dual-joystick

---
+++ components/dual/web/index.html.js
@@ -0,0 +1,26 @@
+import getNipplejsMin from "react-native-game-pad/components/dual/web/nipplejs.min";
+
+const getWebPage = () => {
+  const nipplejs = getNipplejsMin();
+  return `
+    <html>
+      <body>
+        <div id="zone_joystick" style="
+            position: absolute;
+            width: 50%;
+            height: 100%;
+            left: 0;"></div>
+        <div id="zone_joystick2" style="
+            position: absolute;
+            width: 50%;
+            height: 100%;
+            left: 50%;"></div>
+      </body>
+      <script>
+        ${nipplejs}
+      </script>
+    </html>
+    `;
+}
+
+export default getWebPage;
